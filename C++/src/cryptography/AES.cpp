60a3b974671c58f7255ad4a20a512f2df7ff2046
danny-cpp <huudanh2908@gmail.com>
2023-01-02 20:48:32 -0700

    trigger Jenkins

--- C++/src/cryptography/AES.cpp
+++ C++/src/cryptography/AES.cpp
@@ -5,6 +5,7 @@ namespace QPP {
     std::string AES::encrypt(std::string& plain_text, std::string& key, int key_size) {
 
         std::cout << "Encrypting" << std::endl;
+
         return "";
     }
 }
