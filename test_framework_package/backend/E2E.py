9865816165c9cd679e213842bb6f9461ee989115
luwentao <wlu4@ualbera.ca>
2023-01-14 14:34:25 -0700

    add path parameterto encryption test api

--- test_framework_package/backend/E2E.py
+++ test_framework_package/backend/E2E.py
@@ -10,23 +10,12 @@ class E2E:
     def compareStrings_api(self, a: str, b: str) -> bool:
         logger.info("Comparing strings")
         return a == b
-    def encryptionAesTest_api(self, plaintext: str, key: str ) -> str:
+    def encryptionAesTest_api(self, path: str, plaintext: str, key: str ) -> str:
         logger.info("Encrypting string")
-        # os.chdir("..")
-        os.chdir("..")
-        print(os.getcwd())
-        sb.check_call("cd C++", shell=True)
-        
-        #clean the project and make it
-        print("cleaning the project and make it")
-        sb.call("make clean", shell=True)
-        sb.call("make", shell=True)
         #set up timer
         t = threading.Timer(50, self.timeout)
         #run the program
-        print("running the program")
-
-        proc = sb.Popen("./bin/qpp.exe", shell=True, stdin=sb.PIPE, stdout=sb.PIPE,
+        proc = sb.Popen(path, shell=True, stdin=sb.PIPE, stdout=sb.PIPE,
             stderr=sb.PIPE)
 
         #give the program the inputs,(assume here inputs are command line inputs)
