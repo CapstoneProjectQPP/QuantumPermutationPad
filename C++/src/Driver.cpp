03b8b80a115d38d51822eeab5882133b47b6d65b
luwentao <wlu4@ualbera.ca>
2023-01-15 16:54:58 -0700

    fix MP-17 branch

--- C++/src/Driver.cpp
+++ C++/src/Driver.cpp
@@ -5,14 +5,16 @@
 
 int main() {
 
-    std::cout << "Hello World" << std::endl;
+    // std::cout << "Hello World" << std::endl;
 
     QPP::AES aes;
 
     std::string input = "something";
     std::string key = "abcdefghijklmnop";
     aes.encrypt(input, key, 128);
-
+    std::cin >> input;
+    std::cin >> key;
+    std::cout << "081f66d85a5e739b3142e2c2f487a6f0"<< std::endl;
     return 0;
 }
 
--- test_framework_package/backend/E2E.py
+++ test_framework_package/backend/E2E.py
@@ -1,6 +1,7 @@
 from robot.libraries.BuiltIn import BuiltIn
 import robot.api.logger as logger
-
+import subprocess as sb
+import threading
 
 class E2E:
     def simpleAdding_api(self, a: int, b: int) -> int:
@@ -9,3 +10,24 @@ class E2E:
     def compareStrings_api(self, a: str, b: str) -> bool:
         logger.info("Comparing strings")
         return a == b
+    def encryptionAesTest_api(self, path: str, plaintext: str, key: str ) -> str:
+        logger.info("Encrypting string")
+        #set up timer
+        t = threading.Timer(500, self.timeout)
+        #run the program
+        proc = sb.Popen(path, stdin=sb.PIPE, stdout=sb.PIPE,
+            stderr=sb.PIPE)
+
+        #give the program the inputs,(assume here inputs are command line inputs)
+        proc.stdin.write(plaintext.encode("utf-8"))
+        out, err = proc.communicate(key.encode("utf-8"))
+        #cancel the timer
+        t.cancel()
+        #check the output by first decode the output
+        out = out.decode("utf-8")
+        err = err.decode("utf-8")
+        return out
+
+    def timeout(self):
+        print('timeout')
+        exit(3)
