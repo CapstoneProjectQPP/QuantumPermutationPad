5c2afb0887b1bc388c3e200d81afa88181d558c2
luwentao <wlu4@ualbera.ca>
2023-01-25 23:16:17 -0700

    MP-103 Remove unnecessary code

--- C++/src/Driver.cpp
+++ C++/src/Driver.cpp
@@ -15,7 +15,6 @@ int main() {
     std::cin >> key;
     std::string result = aes.encrypt(input, key, 128);
     std::cout << result << std::endl;
-    // std::cout << "081f66d85a5e739b3142e2c2f487a6f0"<< std::endl;
     return 0;
 }
 
