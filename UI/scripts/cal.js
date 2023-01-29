ce00bdaccf8ef4e4e134ab0dc1f97b356ba395d1
Ahmed Refik <refik@ualberta.ca>
2023-01-29 16:13:39 -0700

    Basic weblayout UI

---
+++ UI/scripts/cal.js
@@ -0,0 +1,15 @@
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+setUserName();
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Your name is ${myName}`;
+}
