0dd5ea804c2ace704135983293cdd09a7bf2eea9
Iyury1 <iyurychu@ualberta.ca>
2023-01-30 15:44:04 -0700

    added functions

---
+++ C++/src/cryptography/BraKet.cpp
@@ -0,0 +1,17 @@
+#include "cryptography/BraKet.h"
+
+
+
+namespace QPP {
+
+
+        uint8_t BraKet::getValueAt(int row) {
+
+        }
+
+        void BraKet::setValueAt(int row, int value) {
+
+        }
+
+
+}
