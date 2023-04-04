03027a6095c520dd9930e6112119306b76f33eb9
Ahmed Refik <refik@ualberta.ca>
2023-04-04 14:33:37 -0600

    [MP-144] merging with CLI mutithreading basics

--- CLI/Client.py
+++ CLI/Client.py
@@ -51,8 +51,6 @@ class Client:
         print("OUTGOING QUEUE {}".format(message))
         # self.outgoing_mutex.release()
 
-
-
     def connection_recv(self):
         while True:
             msg = self.s.recv(4096)
